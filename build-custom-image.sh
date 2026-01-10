00e414630b2d644a768d919f83eb8f09fb0452a3
Lin,xiaolong <linxiaolong@niuchart.com>
2026-01-10 16:19:04 +0800

    commit

---
+++ build-custom-image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Custom image name
+IMAGE_NAME="nexus.dev.niuchart.com/docker-hosted/easzlab/kubeasz:3.6.8-custom"
+
+echo "Building and pushing custom image: $IMAGE_NAME"
+# Use docker buildx for multi-arch support
+docker buildx build --platform linux/amd64,linux/arm64 -t "$IMAGE_NAME" -f Dockerfile.custom --push .
+
+echo "Done."
